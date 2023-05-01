ef434acd759f32f3c5aeaf7050f7ca3c22d8b1a3
Liam Kinne <liam@kinne.io>
2023-05-01 23:26:37 +1000

    handle interrupts from both rx0 and rx1

--- src/main.rs
+++ src/main.rs
@@ -286,8 +286,23 @@ mod app {
         isolator_watchdog::spawn_after(100.millis().into()).unwrap();
     }
 
-    #[task(priority = 2, shared = [can, isolator, isolator_wd_fed], binds = CAN1_RX0)]
-    fn can_receive(mut cx: can_receive::Context) {
+    // RX 0 interrupt pending handler.
+    #[task(priority = 2, binds = CAN1_RX0)]
+    fn can_rx0_pending(_: can_rx0_pending::Context) {
+        defmt::trace!("task: can rx0 pending");
+        can_frame_handler::spawn().unwrap();
+    }
+
+    // RX 1 interrupt pending handler.
+    #[task(priority = 2, binds = CAN1_RX1)]
+    fn can_rx1_pending(_: can_rx1_pending::Context) {
+        defmt::trace!("task: can rx1 pending");
+        can_frame_handler::spawn().unwrap();
+    }
+
+    // Process can frames.
+    #[task(priority = 1, shared = [can, isolator, isolator_wd_fed])]
+    fn can_frame_handler(mut cx: can_frame_handler::Context) {
         defmt::trace!("task: can receive");
 
         cx.shared.can.lock(|can| {
